d12fd0e5000969010afaf42c1c08515176ecb0b9
Kamran Azmat <azmat.kamran@gmail.com>
2015-10-31 19:04:32 +0530

    heroku-push

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
  def new_search
  end
   def application_code
-  	 require 'open-uri'
+ 
  	surat_demo_ip = '123.201.85.234'
  	@response = Net::HTTP.get_response("ip-api.com","/json/123.201.85.234").body
  	@json_response = JSON.parse @response
