d74d8c7dea5e98804b4671e081111092c0f626c4
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2015-10-31 22:08:06 +0530

    After dinner

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
  def index
    require 'geoip'
-   @info = GeoIP.new(Rails.root.join("GeoLiteCity.dat")).city(ip_request_params[:host])
-
+   @info = GeoIP.new(Rails.root.join("GeoLiteCity.dat")).city('117.249.81.241')
+   @city = @info.city_name
  end 
 end
