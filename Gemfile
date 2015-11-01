632d2e281259bb593c4c59fb08aea70fdbf9a5f4
Kamran Azmat <azmat.kamran@gmail.com>
2015-11-01 12:00:05 +0530

    one with sqlite

--- Gemfile
+++ Gemfile
@@ -3,14 +3,14 @@ ruby "2.1.5"
 gem 'twitter-bootstrap-rails'
 gem "font-awesome-rails"
 gem  'exotel'
-
+gem 'google_places'
 gem 'geocoder'
-
+gem "rest-client"
 gem 'coffee-script-source', '1.8.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.1'
 # Use sqlite3 as the database for Active Record
-gem 'pg'
+gem 'sqlite3'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,7 @@
 class HomeController < ApplicationController
      before_filter :application_code
-    
-
 
  def index
-
  	  @projects = Search.search(params[:search])
  	  if @projects.count == 0
  	  	if params[:search] != nil && params[:search] != ""
@@ -15,24 +12,26 @@ class HomeController < ApplicationController
  end 
 
 def emergency
-
 	response = Exotel::Call.connect_to_agent(:to => '08039511986', :from => '9512535646', :caller_id => '08039511986', :call_type => 'trans')
-
 end
+
+def hospital
+	@hospitals_json = RestClient.get 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&rankby=distance&types=hospital&key=AIzaSyBUK3jJ6Yh8MB3_2imJUBqAI8Dk8L5Zbds', {:accept => :json}
+end
+
 def stations
 	@geocoder = Geocoder.search(@response["lat"].to_s + " , " + @response["lat"].to_s)
 end
 
 def ngo
 	response = Exotel::Call.connect_to_agent(:to => '08039511986', :from => '8583035546', :caller_id => '08039511986', :call_type => 'trans')
-
 end
 
-  def application_code
- 
+def application_code
  	surat_demo_ip = '123.201.85.234'
  	@response = Net::HTTP.get_response("ip-api.com","/json/123.201.85.234").body
  	@json_response = JSON.parse @response
+
 end
  
 end
